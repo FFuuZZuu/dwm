1e0e513b24614176458bdcdf53c7c649df3c10c2
Callum <spikywebb@gmail.com>
2021-10-03 08:12:42 +0000

    changed dwm colours and modkey

--- config.h
+++ config.h
@@ -5,17 +5,21 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Fira Code:size=10" };
+static const char dmenufont[]       = "Fira Code:size=10";
 static const char col_gray1[]       = "#202020";
-static const char col_gray2[]       = "#362b27";
-static const char col_gray3[]       = "#b69a9a";
-static const char col_gray4[]       = "#ebcece";
-static const char col_cyan[]        = "#b56231";
+static const char col_gray2[]       = "#292423";
+static const char col_gray3[]       = "#362b27";
+static const char col_orange1[]	    = "#8a4e38";
+static const char col_orange2[]     = "#b56231";
+static const char col_orange3[]     = "#de724b";
+static const char col_white1[]      = "#9f7f7f";
+static const char col_white2[]      = "#b69a9a";
+static const char col_white3[]      = "#ebcece";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_white3, col_orange1, col_white1 },
+	[SchemeSel]  = { col_white3, col_orange1,  col_white3  },
 };
 
 /* tagging */
@@ -45,7 +49,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -57,7 +61,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_white1, "-sb", col_orange2, "-sf", col_white3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
